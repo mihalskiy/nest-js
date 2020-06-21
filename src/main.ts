de06c456e97ade6a20a84d24e584dfe91945b0b2
mihalskiy <mihalskiy.rv@gmail.com>
2020-06-21 10:01:00 +0300

    autoload

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import process from "process";
+import * as process from "process";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
