8d6d4b2258c75e470ad4ff5c60019dba51d6f5cf
mihalskiy <mihalskiy.rv@gmail.com>
2020-06-23 03:58:14 +0300

    change DB autoload

--- migrations/1592863371968-Todo.ts
+++ migrations/1592863371968-Todo.ts
@@ -4,11 +4,11 @@ export class Todo1592863371968 implements MigrationInterface {
     name = 'Todo1592863371968'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query("CREATE TABLE `todo` (`id` int NOT NULL AUTO_INCREMENT, `title` varchar(255) NOT NULL, `isComplete` tinyint NOT NULL, `desc` varchar(255) NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB");
+        await queryRunner.query("CREATE TABLE `Todo` (`id` int NOT NULL AUTO_INCREMENT, `title` varchar(255) NOT NULL, `isComplete` tinyint NOT NULL, `desc` varchar(255) NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB");
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query("DROP TABLE `todo`");
+        await queryRunner.query("DROP TABLE `Todo`");
     }
 
 }
